7c1727e5b81cc410bf1067e17ac8aa8a03f9f4dd
Yod-i <yod76656@gmail.com>
2025-09-10 15:13:30 +0500

    added comand activation

--- kpp.lua
+++ kpp.lua
@@ -1,30 +1,48 @@
 require "lib.moonloader"
 
+local kppThread
+local autoShlagbaumActive = false
 local CopsSkins = {280, 281, 282, 283, 284, 285, 286, 288, 76, 306, 307, 309, 302, 310, 311, 303, 304, 305, 266, 267, 265}
-local bratki = {'Lik_Glars', 'Oliver_Jones', 'Noah_Weissberg', 'Rocco_Parondi', 'Sereja_Mavrodi', 'Yuki_Blare', 'Solomon_Bogdanov'}
+local bratki = {'Lik_Glars', 'Oliver_Jones', 'Noah_Weissberg', 'Rocco_Parondi', 'Sereja_Mavrodi', 'Yuki_Blare', 'Solomon_Bogdanov', 'Molly_Baila', 'Vegas_Young', 'Liya_Carter', 'Shatter_Bones'}
 
 
 function main()
     while not isSampAvailable() do wait (0) end
+
+    sampRegisterChatCommand('kpp', IsCopSkin)
+
     while true do
-        IsCopSkin()
+
         wait(0)
     end
 end
 
 function IsCopSkin()
-    if isCharOnFoot(PLAYER_PED) then
-        local stealedfunc = GetCopsInRadius(35)
-        if stealedfunc then
-            printStringNow('Raise the barrier!', 400)
-            setGameKeyState(21,255)
-            wait(100)
-            setGameKeyState(21,255)
-        end
+    autoShlagbaumActive = not autoShlagbaumActive
+    if autoShlagbaumActive then
+        kppThread = lua_thread.create(function ()
+            while true do
+                if autoShlagbaumActive and isCharOnFoot(PLAYER_PED) then
+                    if IsCopsInRadius(35) then
+                        printStringNow('Raise the barrier!', 400)
+                        setGameKeyState(21,255)
+                        wait(100)
+                        setGameKeyState(21,255)
+                    end
+                end
+                wait(0)
+            end
+        end)
+    else
+        kppThread:terminate()
     end
+    sampAddChatMessage(autoShlagbaumActive and 'Авто шлагбаум включен' or 'Авто шлагбаум выключен', -1)
+
+    
+    
 end
 
-function GetCopsInRadius(radius)
+function IsCopsInRadius(radius)
 
     for _, pedHandle in ipairs(getAllChars()) do
         local pedCar = storeCarCharIsInNoSave(pedHandle)
@@ -34,7 +52,7 @@ function GetCopsInRadius(radius)
             local pedNickName = sampGetPlayerNickname(pedID)
             for _, value in ipairs(bratki) do
                 if pedNickName == value then
-                    if isCharInAnyCar(pedHandle) and getDistanceBetweenPlayerAndCar(pedCar, radius) then
+                    if isCharInAnyCar(pedHandle) and IsCarInRadius(pedCar, radius) then
                         return true
                     end
                 end
@@ -43,15 +61,17 @@ function GetCopsInRadius(radius)
 
         for _, value in ipairs(CopsSkins) do -- проверка на то, Коп ли это
             if getCharModel(pedHandle) == value then
-                if isCharInAnyCar(pedHandle) and getDistanceBetweenPlayerAndCar(pedCar, radius) then
+                if isCharInAnyCar(pedHandle) and IsCarInRadius(pedCar, radius) then
                     return true
                 end
             end
         end
 
         if isCharInAnyCar(pedHandle) then
-            if isCharInAnyCar(pedHandle) and getDistanceBetweenPlayerAndCar(pedCar, radius) then
-                return true
+            if getCarModel(pedCar) == 433 then -- проверка на то, Barracks ли это
+                if isCharInAnyCar(pedHandle) and IsCarInRadius(pedCar, radius) then
+                    return true
+                end
             end
         end
     end
@@ -59,10 +79,10 @@ function GetCopsInRadius(radius)
     return false
 end
 
-function getDistanceBetweenPlayerAndCar(pedCarParameter, radius)
+function IsCarInRadius(pedCarParameter, radius)
     local x1, y1, z1 = getCharCoordinates(PLAYER_PED)
     local x2, y2, z2 = getCarCoordinates(pedCarParameter)
-    if getDistanceBetweenCoords3d(x1, y1, z1, x2, y2, z2) <= radius and z1 - z2 <= 4 and getCarSpeed(pedCarParameter) > 1 then
+    if getDistanceBetweenCoords3d(x1, y1, z1, x2, y2, z2) <= radius and z1 - z2 <= 4 and z2 - z1 <= 4 and getCarSpeed(pedCarParameter) >= 1 then
         return true
     end
 end
